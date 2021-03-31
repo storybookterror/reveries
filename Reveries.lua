171820ff3f171d295e5be3013cc842706f2633e5
StorybookTerror <storybookterror@gmail.com>
2021-03-31 13:17:47 -0700

    Release version 0.7

--- Reveries.lua
+++ Reveries.lua
@@ -2,7 +2,7 @@ local LAM2 = LibAddonMenu2
 
 local RV = {
     name = "Reveries",
-    version = "0.6"
+    version = "0.7"
 }
 
 -- Automatically ignore all messages in these maps (like solo areas)
